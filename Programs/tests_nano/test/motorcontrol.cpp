5cf5ed197b8b9066566ea42d188cba1cdda9b04a
Phil-obed <philemonobjnr@gmail.com>
2025-07-24 16:27:38 +0000

    test 1 init

--- Programs/tests_nano/test/motorcontrol.cpp
+++ Programs/tests_nano/test/motorcontrol.cpp
@@ -1,10 +1,38 @@
 #include <Arduino.h>
 #include "MotorControl_L293D.hpp"
 
-Bot bot(uint8_t Rf, uint8_t Rb, uint8_t Lf, uint8_t Lb, uint8_t enR, uint8_t enL);
+
+#define Rf 13
+#define Rb 12
+#define Lf 8
+#define Lb 7
+#define enR 11
+#define enL 10
+
+Bot bot(Rf, Rb, Lf, Lb, enR, enL);
+
+uint8_t fast = 255;
+uint8_t mid = (255/2) + 10;
+uint8_t slow = 10;
 
 void setup() {
+    bot.init();
+    bot.stop(0);
 }
 
 void loop() {
+    bot.forward(1, fast);
+    bot.reverse(1, fast);
+    bot.left(1, fast);
+    bot.right(1, fast);
+
+    bot.forward(1, mid);
+    bot.reverse(1, mid);
+    bot.left(1, mid);
+    bot.right(1, mid);
+
+    bot.forward(1, slow);
+    bot.reverse(1, slow);
+    bot.left(1, slow);
+    bot.right(1, slow);
 }
