f9fc4c9cf4f8cc023251dd759a7a107e0e3ae201
Phil-obed <philemonobjnr@gmail.com>
2025-07-31 17:01:06 +0000

    u

--- Programs/test-arduinoFramework.nano/lib/ObstacleAvoidance/ObstacleAvoidance.hpp
+++ Programs/test-arduinoFramework.nano/lib/ObstacleAvoidance/ObstacleAvoidance.hpp
@@ -1,35 +1,42 @@
 #pragma once
 #include <Arduino.h>
+#include "MotorControl_L293D.hpp"
 
-class Bot
-{
-private:
-    uint8_t _motors = 2;
-    uint8_t _speed;
-    uint8_t _Rf;
-    uint8_t _Rb;
-    uint8_t _Lf;
-    uint8_t _Lb;
-    uint8_t _enR;
-    uint8_t _enL;
+class ObstacleAvoidance {
+    private:
+        struct Sensor {
+            uint8_t trig;
+            uint8_t echo;
+            float distance;
+            unsigned long startTime;
+            bool triggered;
+        };
 
-    uint8_t motorPins[4];
+        Bot* _bot;
 
-    uint8_t _current_Speed;
-    uint8_t _target_Speed;
-    uint8_t _ramp_Step;
-    uint8_t _ramp_Interval;
-    unsigned long _last_UpdatedT;
-    uint8_t _current_Pattern;
+        Sensor sensors[3];
+        enum SensorIndex { FL = 0, FC = 1, FR = 2 };
+        uint8_t _currentSensor = 0;
 
-public:
-    Bot(uint8_t Rf, uint8_t Rb, uint8_t Lf, uint8_t Lb, uint8_t enR, uint8_t enL);
-    void init();
-    void move(uint8_t pattern);
-    void stop(uint8_t rampStep);
-    void forward(uint8_t accSpeed, uint8_t speed);
-    void reverse(uint8_t accSpeed, uint8_t speed);
-    void right(uint8_t accSpeed, uint8_t speed);
-    void left(uint8_t accSpeed, uint8_t speed);
-    void updateSpeed();
+        unsigned long _lastStateUpdate = 0;
+        unsigned long _sensorCycleDelay = 5;  // 5ms between sensors
+        unsigned long _sensorTimeout = 30000; // 30ms timeout for echo
+
+        const float STOP_DIST = 20.0;
+        const float AVOID_DIST = 35.0;
+
+        const uint8_t _speed = 180;
+        const uint8_t _accStep = 10;
+
+        void triggerSensor(Sensor& sensor);
+        void readEcho(Sensor& sensor);
+
+    public:
+        ObstacleAvoidance(Bot* bot,
+                        uint8_t trigFL, uint8_t echoFL,
+                        uint8_t trigFC, uint8_t echoFC,
+                        uint8_t trigFR, uint8_t echoFR);
+
+        void begin();
+        void update();
 };
